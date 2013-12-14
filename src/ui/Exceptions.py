e8ac4558e3a69e355953ae243a16eedfc4512bb6
ClaudineLaurendeau <claudine.laurendeau@mrn.gouv.qc.ca>
2013-12-14 17:36:49 -0500

    commit excep

--- src/ui/Exceptions.py
+++ src/ui/Exceptions.py
@@ -1,123 +1,131 @@
 Fichier sur les Exceptions
 
-
-    Class Exc_depl_pions(Exceptions):
-
-        '''Exceptions qui va gerer les contraintes concernant les deplacememnts
-            possibles des pions.'''
-
-        def__init__(self, message):
-
-             '''Constructor'''
-
-             super().__init__()
-             self.message = message
-
-
-        def__repr__(self):
-
-            return "Erreur de deplacement du pions: " + self.msg
+class Exception (objet):
+    
+    def__init__(self):
+        pass
+class Exc_jeu (Exception):
+    
+    def __init__ (self, color):
+        self.color = color
         
+    def tour_jouer(self):
+        try color == 
+Class Exc_depl_pions(Exceptions):
+
+    '''Exceptions qui va gerer les contraintes concernant les deplacememnts
+        possibles des pions.'''
+    
+    def__init__(self, message):
+    
+         '''Constructor'''
+    
+         super().__init__()
+         self.message = message
 
 
-        Class Exc_depl_roi(Exceptions):
+    def__repr__(self):
 
-            '''
-         Exception qui va gerer les contraintes concernant les deplacements
-         possibles du roi.'''
+        return "Erreur de deplacement du pions: " + self.msg
 
+Class Exc_depl_roi(Exceptions):
 
-        def__init__(self, message):
-
-             '''Constructor'''
-
-             super().__init__()
-             self.message = message
+        '''
+     Exception qui va gerer les contraintes concernant les deplacements
+     possibles du roi.'''
 
 
-        def__repr__(self):
+    def__init__(self, message):
 
-            return "Erreur de deplacement du roi: " + self.msg
+         '''Constructor'''
 
-
-          Class Exc_depl_dame(Exception)
-
-          ''' Exceptions qui va etre gerer les contraintes concernant
-               les deplacements possibles de la dame.'''
-
-           def__init__(self, message):
-
-             '''Constructor'''
-
-             super().__init__()
-             self.message = message
+         super().__init__()
+         self.message = message
 
 
-        def__repr__(self):
+    def__repr__(self):
 
-            return "Erreur de deplacement de la dame: " + self.msg
-
-
-        Class Exc_depl_tour(Exception):
-
-            '''Excpeption qui va gerer les contraintes concernant
-            les deplacements possibles de la tour.'''
-
-            
-        def__init__(self, message):
-
-             '''Constructor'''
-
-             super().__init__()
-             self.message = message
+        return "Erreur de deplacement du roi: " + self.msg
 
 
-        def__repr__(self):
+Class Exc_depl_dame(Exception)
 
-            return "Erreur de deplacement de la tour: " + self.msg
+      ''' Exceptions qui va etre gerer les contraintes concernant
+           les deplacements possibles de la dame.'''
 
+       def__init__(self, message):
 
-    Class Exc_depl_cavalier(Exception):
+         '''Constructor'''
 
-            '''Exception qui va gerer les contraintes concernant les
-              deplacements possibles du cavalier'''
-
-        def__init__(self, message)
-
-           '''Constructor'''
-
-           super().__init__()
-           self.msg = message
-
-        delf__repr__(self):
-            return "Erreur de deplacement du cavalier: " + self
-
-    Class_Exc_depl_fou(Exception):
-
-            '''Exception qui va gerer les contraintes concernant les deplacemnts
-               possibles du fou. '''
-
-        def__init__(self, message):
-
-            '''constructor
-            '''
-            super().__init__()
-            self.message = message
-
-       def__repr__(self):
-           return "Erreur de deplacemnt du fou: " + self.msg
+         super().__init__()
+         self.message = message
 
 
-        Class Exc_couleur_pieces(Exceptions):
+    def__repr__(self):
 
-            '''Exception qui va gerer si le joueur joue les pieces adverses.'''
+        return "Erreur de deplacement de la dame: " + self.msg
 
-            def__init__(self, message):
 
-                ''' Constructor
-                '''
-                super().__init__()
-                self.msg = message
+Class Exc_depl_tour(Exception):
 
-            def__repr__(self):
-                return " Action interdite: " + self.msg
+        '''Excpeption qui va gerer les contraintes concernant
+        les deplacements possibles de la tour.'''
+
+        
+    def__init__(self, message):
+
+         '''Constructor'''
+
+         super().__init__()
+         self.message = message
+
+
+    def__repr__(self):
+
+        return "Erreur de deplacement de la tour: " + self.msg
+
+
+Class Exc_depl_cavalier(Exception):
+
+        '''Exception qui va gerer les contraintes concernant les
+          deplacements possibles du cavalier'''
+
+    def__init__(self, message)
+
+       '''Constructor'''
+
+       super().__init__()
+       self.msg = message
+
+    delf__repr__(self):
+        return "Erreur de deplacement du cavalier: " + self
+
+Class_Exc_depl_fou(Exception):
+
+        '''Exception qui va gerer les contraintes concernant les deplacemnts
+           possibles du fou. '''
+
+    def__init__(self, message):
+
+        '''constructor
+        '''
+        super().__init__()
+        self.message = message
+
+   def__repr__(self):
+       return "Erreur de deplacemnt du fou: " + self.msg
+
+
+Class Exc_couleur_pieces(Exceptions):
+
+    '''Exception qui va gerer si le joueur joue les pieces adverses.'''
+
+    def__init__(self, message):
+
+        ''' Constructor
+        '''
+        super().__init__()
+        self.msg = message
+
+    def__repr__(self):
+        return " Action interdite: " + self.msg
